f3280eaaec999cb21197832e5a35b632ac31d4ab
sherlock <sherlock@sherlock-MS-7693.(none)>
2013-09-17 05:36:29 +0530

    specific div overlayed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 $(function(){
 	$("#add").on('click', function(){
-		$(".loadercontainer").show();
+		var container = $(".text-container");
+		var width = container.width();
+		var height = container.height();
+		var offset =  container.offset();
+		var top = offset.top;
+		var left = offset.left;
+		$("#loaderdiv").css({
+			width:width,
+			height:height,
+			top:top,
+			left:left
+		}).parent().show();
 		setTimeout(function(){
 			$(".loadercontainer").hide();
 		},3000);
